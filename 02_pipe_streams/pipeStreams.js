70ab815f8aee6a45fc680c71ea0e215210c2d445
Dmitry Gladkikh <abdurahmanus@gmail.com>
2017-01-18 15:57:14 +0300

    reorganize folders

--- 02_pipe_streams/pipeStreams.js
+++ 02_pipe_streams/pipeStreams.js
@@ -3,4 +3,4 @@ var zlib = require("zlib");
 var fs = require("fs");
 
 //request("http://www.pluralsight.com/").pipe(fs.createWriteStream("pluralsight.html"));
-request("http://www.pluralsight.com/").pipe(zlib.createGzip()).pipe(fs.createWriteStream("./data/pluralsight.html.gz"));
+request("http://www.pluralsight.com/").pipe(zlib.createGzip()).pipe(fs.createWriteStream("pluralsight.html.gz"));
